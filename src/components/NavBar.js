9494350a9a32fc35880c65b2a508a147dcaaf0e7
autoex <autoex@gmail.com>
2021-10-08 18:47:14 +0300

    add styled components library

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const Styles = styled.div`
+a, .navbar-dark .navbar-nav .nav-link {
+text-decoration:none;
+color: #adb1b8;
+&:hover {
+color: white}
+}`
 
 const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand='lg' collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                <Navbar.Collapse id='responsive-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link>
-                            <Link to='/'>Home</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to='/users'>Users</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to='/about'>About</Link>
-                        </Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button variant='primary' className='me-2'>Log in</Button>
-                        <Button variant='primary'>Sing out</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Styles>
+            <Navbar bg="dark" variant="dark" expand='lg' collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                    <Navbar.Collapse id='responsive-navbar-nav'>
+                        <Nav className='me-auto'>
+                            <Nav.Link>
+                                <Link to='/'>Home</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to='/users'>Users</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to='/about'>About</Link>
+                            </Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Button variant='primary' className='me-2'>Log in</Button>
+                            <Button variant='primary'>Sing out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Styles>
 
     );
 };
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,19 +6,25 @@ import img3 from '../img/photo5402186058361320574.jpg'
 import img4 from '../img/photo5402186058361320575.jpg'
 
 const Slider = () => {
+    const imgCaption = '#RBRHondaTurkey';
+    const carouselItemStyles = {height: '600px'}
     return (
         <Carousel>
-            <Carousel.Item>
+            <Carousel.Item style={carouselItemStyles}>
                 <img src={img1} alt="" className='d-block w-100'/>
+                <Carousel.Caption><h4>{imgCaption}</h4></Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={carouselItemStyles}>
                 <img src={img2} alt="" className='d-block w-100'/>
+                <Carousel.Caption><h4>{imgCaption}</h4></Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={carouselItemStyles}>
                 <img src={img3} alt="" className='d-block w-100'/>
+                <Carousel.Caption><h4>{imgCaption}</h4></Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={carouselItemStyles}>
                 <img src={img4} alt="" className='d-block w-100'/>
+                <Carousel.Caption><h4>{imgCaption}</h4></Carousel.Caption>
             </Carousel.Item>
 
         </Carousel>
