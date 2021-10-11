b2ca0f07f208cec5c66d43c586549edb593f5837
autoex <autoex@gmail.com>
2021-10-11 14:42:02 +0300

    add Cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import Slider from "./Slider";
+import {Card, Col, Container, Row} from "react-bootstrap";
+import img4 from '../img/photo5402186058361320575.jpg'
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
     return (
         <div>
             <Slider/>
+            <Container className='mt-4'>
+                <Row>
+                    <Col>
+                        <Card style={{width: '18rem'}}>
+                            <Card.Img variant='top' src={img4}/>
+                            <Card.Body>
+                                <Card.Title>Title</Card.Title>
+                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem,
+                                    totam!</Card.Text>
+                                <Button variant='primary'>Learn more</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col><Col>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant='top' src={img4}/>
+                        <Card.Body>
+                            <Card.Title>Title</Card.Title>
+                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem,
+                                totam!</Card.Text>
+                            <Button variant='primary'>Learn more</Button>
+                        </Card.Body>
+                    </Card>
+                </Col><Col>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant='top' src={img4}/>
+                        <Card.Body>
+                            <Card.Title>Title</Card.Title>
+                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem,
+                                totam!</Card.Text>
+                            <Button variant='primary'>Learn more</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
